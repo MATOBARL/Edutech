3004b0afa57a11c9fb730076e7c55e683b4af0f5
MATOBARL <ma.tobarl@duocuc.cl>
2025-05-08 09:50:50 -0400

    init commit

---
+++ msvc-profesor/src/test/java/com/edutech/msvc/profesor/MsvcProfesorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.profesor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProfesorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
